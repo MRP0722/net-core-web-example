88065267bb241b74985a523ac021d79b6ee74a77
RuiPeng Mei <BossMRP@outlook.com>
2019-09-26 14:51:42 +0800

    添加项目文件。

---
+++ EmployeeSys/Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using EmployeeSys.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeSys.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(EmployeeData.Employees);
+        }
+
+        [HttpGet]
+        public IActionResult AddEmployee()
+        {
+            EmployeeViewModel model = new EmployeeViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(EmployeeViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                EmployeeData.Employees.Add(model);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ EmployeeSys/Models/EmployeeData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeSys.Models
+{
+    public class EmployeeData
+    {
+        public static List<EmployeeViewModel> Employees { get; set; } = new List<EmployeeViewModel>();
+    }
+}
